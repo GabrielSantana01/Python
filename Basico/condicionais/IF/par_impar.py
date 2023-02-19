fb1df3a12bd8e5e863b2454b5c310b16b82292f5
Gabriel Santana <gabrielsa45@gmail.com>
2023-02-19 11:07:52 -0300

    Create par_impar.py

---
+++ Basico/condicionais/IF/par_impar.py
@@ -0,0 +1,6 @@
+entrada = int(input('insira um numero!'))
+
+if (entrada % 2) == 0 :
+    print(f'o numero inserido foi:{entrada} e ele é par')
+else:
+    print(f'o numero inserido foi:{entrada} e ele é impar')
