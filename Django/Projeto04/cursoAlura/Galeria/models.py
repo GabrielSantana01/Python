b3dfd4dce114cf33c8cdc8866f6f65a1191fb324
GabrielSantana01 <gabrielsa45@gmail.com>
2024-04-23 18:04:39 -0300

    primeira tabela

--- Django/Projeto04/cursoAlura/Galeria/models.py
+++ Django/Projeto04/cursoAlura/Galeria/models.py
@@ -1,10 +1,4 @@
 from django.db import models
 
-class teste01(models.Model):
-    tagname =  models.CharField(max_length=50)   
-
-    def __str__(self):
-        return self.tagname
-    
 
 
--- Django/Projeto04/cursoAlura/Galeria/views.py
+++ Django/Projeto04/cursoAlura/Galeria/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from .models import teste01
 def index(request):
     return render(request, 'index.html')
 
 def imagem(request):
     return render(request, 'imagem.html')
 
-def minha_view(request):
-    registros = teste01.objects.using('default').all()
-    return render(request, 'meu_template.html', {'registros': registros})
